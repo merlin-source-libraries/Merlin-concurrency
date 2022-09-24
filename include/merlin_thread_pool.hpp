da77fc47f5ee3dc74b44c6c5d885e05bf42ade1b
Raphaël Lefèvre <raphael.lefevre1@gmail.com>
2022-09-24 14:01:41 +0200

    Add a thread pool implementation

---
+++ include/merlin_thread_pool.hpp
@@ -0,0 +1,146 @@
+#ifndef MERLIN_THREAD_POOL_HPP
+#define MERLIN_THREAD_POOL_HPP
+
+/*!
+ * \file merlin_thread_pool.hpp
+ * \brief A thread pool implementation.
+ * \author Raphaël Lefèvre
+*/
+
+#include <queue>              // std::queue
+#include <vector>             // std::vector
+#include <thread>             // std::thread
+#include <atomic>             // std::atomic
+#include <mutex>              // std::mutex
+#include <condition_variable> // std::condition_variable
+
+/*!
+ * \namespace merl
+ * \brief Merlin namespace.
+ */
+namespace merl
+{
+    /*!
+     * \brief Structure to define a task.
+     *
+     * Any task given to the thread pool must inherits the Task structure.
+     */
+    struct task
+    {
+        virtual ~task() = default;
+        /*!
+         * \brief The task to be executed.
+         */
+        virtual void run() = 0;
+    };
+
+    /*!
+     * \brief A thread pool.
+     */
+    class thread_pool final
+    {
+        private:
+            const std::size_t threads_count_;
+
+            std::queue<task*> tasks_pool_;
+            mutable std::mutex guardian_;
+
+            std::vector<std::thread> threads_pool_;
+            std::atomic<bool> alive_;
+
+            std::atomic<std::size_t> running_tasks_count_;
+        
+            std::condition_variable thread_spin_cv_;
+
+        public:
+            /*!
+             * \brief The policy to adopt when we stop the thread pool.
+             */
+            enum STOP_POLICY {SYNC /*!< Wait (block) until all threads have finished. */, ASYNC /*!< Delegate the threads synchronization (returns directly). See joinAll(). */};
+            /*!
+             * \brief Status to describe whether the thread pool is started or not.
+             */
+            enum STATUS {UP /*!< The thread pool is up (already started). */, DOWN /*! The thread pool is down (stopped or never started). <*/};
+
+            /*!
+             * \brief Constructor.
+             * \param[in] threads_count The desired number of threads -- Default: `std::thread::hardware_concurrency()`
+             */
+            thread_pool(std::size_t threads_count = std::thread::hardware_concurrency());
+            ~thread_pool();
+
+            /*!
+             * \brief Start the thread pool.
+             *
+             * Bring up the threads and start the spinning.
+             */
+            void start();
+            /*!
+             * \brief Stop the thread pool.
+             * \param[in] sp The \ref ThreadPool::STOP_POLICY "stop policy"
+             *
+             * Stop the threads spinning and lead the threads to termination.
+             */
+            void stop(STOP_POLICY sp = SYNC);
+            /*!
+             * \brief Get the current status of the thread pool.
+             * \return \ref ThreadPool::UP "UP" if already started, \ref ThreadPool::DOWN "DOWN" otherwise
+             */
+            STATUS status() const;
+            /*!
+             * \brief Synchronize the threads termination.
+             *
+             * This function can be called if the thread pool was \ref ThreadPool::ASYNC "asynchronously" \ref stop() "stopped".
+             *
+             * \note This function does nothing if the thread pool is still alive.
+             */
+            void join_all();
+            /*!
+             * \brief Add a task to the tasks queue.
+             * \param[in] t The task to add
+             *
+             * \note A task can be added at any time, whether the thread pool is up or not.
+             */
+            void add_task(task * t);
+            /*!
+             * \brief Clear the tasks queue.
+             *
+             * All the pending tasks are removed and will not be executed.
+             *
+             * \note The running tasks (already handled by a thread) will not be affected. They will keep running until completion.
+             */
+            void clear();
+            /*!
+             * \brief Check if there are pending tasks.
+             * \return `false` if the task queue is empty, `true` otherwise
+             */
+            bool is_pending() const;
+            /*!
+             * \brief Get the number of pending tasks.
+             * \return The number of pending tasks
+             */
+            std::size_t pending() const;
+            /*!
+             * \brief Check if there are running tasks.
+             * \return `true` is there is at least one running task, `false` otherwise
+             */
+            bool is_running() const;
+            /*!
+             * \brief Get the number of running tasks.
+             * \return The number of running tasks
+             */
+            std::size_t running() const;
+            /*!
+             * \brief Wait (block) until all pending tasks are handled and finished.
+             *
+             * This function returns when there is no pending nor running tasks left (i.e. `(!is_pending() && !is_running()) == true`).
+             */
+            void wait_for_idle() const;
+
+        private:
+            void spin(); // Each thread execute the spin() function with its own control block.
+            void clean_up();
+    };
+}
+
+#endif // MERLIN_THREAD_POOL_HPP
---
+++ src/merlin_thread_pool.cpp
@@ -0,0 +1,124 @@
+#include <merlin_thread_pool.hpp>
+
+namespace merl
+{
+    thread_pool::thread_pool(std::size_t threads_count) : threads_count_(threads_count), alive_(false), running_tasks_count_(0)
+    {}
+    thread_pool::~thread_pool()
+    {
+        this->stop(SYNC);
+    }
+
+    void thread_pool::start()
+    {
+        // Do nothing if already alive.
+        if(!alive_)
+        {
+            // Empty the threads pool if it is not
+            // i.e. stop() was called with STOP_POLICY::ASYNC.
+            if(!threads_pool_.empty())
+            {
+                this->clean_up();
+            }
+
+            // Start
+            alive_ = true;
+            for(std::size_t i = 0; i < threads_count_; ++i)
+                threads_pool_.push_back(std::thread(&thread_pool::spin, this));
+        }
+    }
+    void thread_pool::stop(STOP_POLICY sp)
+    {
+        {
+            std::lock_guard<std::mutex> locker(guardian_); // Lock to make the condition_variable's predicate evaluation safe (let it finish and return before the wait() releases the mutex -> Prevent threads to miss the notify_all()).
+            alive_ = false;
+        }
+        thread_spin_cv_.notify_all();
+
+        if(sp == SYNC && !threads_pool_.empty())
+        {
+            this->clean_up();
+        }
+    }
+    thread_pool::STATUS thread_pool::status() const
+    {
+        return (alive_ ? UP : DOWN);
+    }
+    void thread_pool::join_all()
+    {
+        if(!alive_ && !threads_pool_.empty())
+        {
+            this->clean_up();
+        }
+    }
+    void thread_pool::add_task(task * t)
+    {
+        std::lock_guard<std::mutex> locker(guardian_);
+        tasks_pool_.push(t);
+        if(alive_)
+            thread_spin_cv_.notify_one();
+    }
+    void thread_pool::clear()
+    {
+        std::queue<task*> tmp;
+        guardian_.lock();
+        tasks_pool_.swap(tmp);
+        guardian_.unlock();
+    }
+    bool thread_pool::is_pending() const
+    {
+        std::lock_guard<std::mutex> locker(guardian_);
+        return !tasks_pool_.empty();
+    }
+    std::size_t thread_pool::pending() const
+    {
+        std::lock_guard<std::mutex> locker(guardian_);
+        return tasks_pool_.size();
+    }
+    bool thread_pool::is_running() const
+    {
+        return running_tasks_count_;
+    }
+    std::size_t thread_pool::running() const
+    {
+        return running_tasks_count_;
+    }
+    void thread_pool::wait_for_idle() const
+    {
+        while(is_pending() || is_running())
+            std::this_thread::yield();
+    }
+
+    void thread_pool::spin()
+    {
+        task * t;
+        while(alive_)
+        {
+            std::unique_lock<std::mutex> lk(guardian_);
+            thread_spin_cv_.wait(lk, [&](){return !alive_ || !tasks_pool_.empty();});
+            
+            // At this point, if "alive_ == true" then tasks_pool_ cannot be empty (the lock was reacquired before evaluating the predicate).
+
+            if(alive_)
+            {
+                t = tasks_pool_.front();
+                tasks_pool_.pop();
+                lk.unlock();
+
+                ++running_tasks_count_;
+                t->run();
+                --running_tasks_count_;
+            }
+            // No need to "else lk.unlock()" here since the std::unique_lock will already release the mutex at destruction.
+        }
+    }
+    void thread_pool::clean_up()
+    {
+        for(std::thread & th : threads_pool_)
+        {
+            if(th.joinable())
+                th.join();
+        }
+        threads_pool_.clear();
+    }
+}
